4e3e57e8af0d689a41274aec6f7356bde3494437
Eliezer Ong <ongeliezer99@gmail.com>
2022-11-03 20:17:39 +0800

    added parameters for aqp

--- preprocessing.py
+++ preprocessing.py
@@ -1,18 +1,52 @@
 import psycopg2
 
+parameters = {
+
+    'enable_async_append' : 'ON',
+    'enable_bitmapscan' : 'ON',
+    'enable_gathermerge' : 'ON',
+    'enable_hashagg' : 'ON',
+    'enable_hashjoin' : 'ON',
+    'enable_incremental_sort' : 'ON',
+    'enable_indexscan' : 'ON',
+    'enable_indexonlyscan' : 'ON',
+    'enable_indexonlyscan' : 'ON',
+    'enable_memoize' : 'ON',
+    'enable_mergejoin' : 'ON',
+    'enable_nestloop' : 'ON',
+    'enable_parallel_append' : 'ON',
+    'enable_parallel_hash' : 'ON',
+    'enable_partition_pruning' : 'ON',
+    'enable_partitionwise_join' : 'ON',
+    'enable_partitionwise_join' : 'ON',
+    'enable_seqscan' : 'ON',
+    'enable_sort' : 'ON',
+    'enable_tidscan' : 'ON'
+
+}
+
 #establishing the connection
-conn = psycopg2.connect(
-   database="TPC-H", user='postgres', password='admin123', host='localhost'
-)
+def connect():
 
-print("Connecting to sql database...")
-cursor = conn.cursor()
+    conn = psycopg2.connect(
+    database="TPC-H", user='postgres', password='admin123', host='localhost'
+    )
 
+    print("Connecting to sql database...")
+    cursor = conn.cursor()
+    print("Successfully connected to sql database!")
+    return cursor
+
+cursor = connect()
 print("Executing SQL query")
-cursor.execute("SELECT * FROM customer")
+cursor.execute("EXPLAIN ANALYSE SELECT * FROM customer")
 
 print("SQL query executed")
-print(cursor.fetchone())
+
+cost = cursor.fetchone()
+print(cost[0])
+
+
 
 
 
